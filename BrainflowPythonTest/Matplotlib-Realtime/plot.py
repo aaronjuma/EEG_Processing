61ae4783fd39144ff5518837707e6b54c85adfd4
Aaron Jumarang <kapj.jumarang@gmail.com>
2024-05-14 11:21:47 -0400

    Added comments

--- BrainflowPythonTest/Matplotlib-Realtime/plot.py
+++ BrainflowPythonTest/Matplotlib-Realtime/plot.py
@@ -5,16 +5,24 @@ from numpy import genfromtxt
 
 def main():
 
+    # Read data from csv
     data = genfromtxt('test.csv', delimiter=',')
+
+    # Turn columns to row (easier to manipulate)
     data = np.transpose(data)
+
+    # Get amount of channels
     channels = len(data) - 1
 
+    # Set up figure
     fig = plt.figure()
     ax = fig.subplots(channels)
 
+    # Seperate the time data from EEG channels
     x = data[0]
     y = data[1:channels+1]
 
+    # Plot
     for i in range(channels):
         ax[i].plot(x, y[i], color = 'tab:red')
     plt.show()
